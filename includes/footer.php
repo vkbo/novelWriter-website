c22dee3e840df6fec66f98d6aa0537ac6d14e1f5
Veronica K. B. Olsen <1619840+vkbo@users.noreply.github.com>
2021-02-04 17:27:21 +0100

    Added icons and update styles

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,9 @@
+</div>
 </main>
 <footer>
-<div class="text-box">Copyright &copy; 2018-2021 Veronica Berglyd Olsen</div>
+  <div class="text-box">
+    Copyright &copy; 2018&ndash;<?php echo date("Y"); ?> Veronica Berglyd Olsen
+  </div>
 </footer>
 </body>
 </html>
--- includes/header.php
+++ includes/header.php
@@ -8,17 +8,25 @@
   <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@700&display=swap" rel="stylesheet"> 
   <link rel="stylesheet" type="text/css" href="css/normalize.css">
   <link rel="stylesheet" type="text/css" href="css/styles.css">
+  <link rel="icon" href="images/icon-novelwriter-32.png" sizes="32x32">
+  <link rel="icon" href="images/icon-novelwriter-192.png" sizes="192x192">
+  <link rel="apple-touch-icon" href="images/icon-novelwriter-192.png">
+  <meta name="msapplication-TileImage" content="images/icon-novelwriter-192.png">
 </head>
 <body>
 <header>
-  <div id="top-banner">Want to contribute? Fork novelWriter on <a href="https://github.com/vkbo/novelWriter">GitHub</a></div>
-  <div class="text-box">
+  <div id="top-banner">
+    <div class="text-box">
+      Want to contribute? Fork novelWriter on <a href="https://github.com/vkbo/novelWriter">GitHub</a>
+    </div>
+  </div>
+  <div id="header-content">
     <h1>novelWriter</h1>
     <div id="sub-title">A markdown-like editor for novels</div>
   </div>
 </header>
 <nav>
-  <div class="text-box">
+  <div id="nav-box">
     <ul>
       <li><a href="index.php">Overview</a></li>
       <li><a href="https://novelwriter.readthedocs.io/">Documentation</a></li>
@@ -29,3 +37,4 @@
   </div>
 </nav>
 <main>
+<div id="content">
