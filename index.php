9dee9587d8676cc30d10d6f24408013c958e0984
Veronica K. B. Olsen <1619840+vkbo@users.noreply.github.com>
2021-03-14 18:22:43 +0100

    Release 1.2

--- index.php
+++ index.php
@@ -5,17 +5,17 @@
   // ========
 
   // Latest Stable Release
-  $nwStableVers   = "1.1.1";
-  $nwStableDate   = "2021-02-21";
-  $nwStableNotes  = "https://github.com/vkbo/novelWriter/releases/tag/v1.1.1";
-  $nwStableMinMac = "https://github.com/vkbo/novelWriter/releases/download/v1.1.1/novelWriter-1.1.1-minimal-darwin.zip";
-  $nwStableMinLnx = "https://github.com/vkbo/novelWriter/releases/download/v1.1.1/novelWriter-1.1.1-minimal-linux.zip";
-  $nwStableMinWin = "https://github.com/vkbo/novelWriter/releases/download/v1.1.1/novelWriter-1.1.1-minimal-win.zip";
-  $nwStableSrcZip = "https://github.com/vkbo/novelWriter/archive/v1.1.1.zip";
-  $nwStableSrcTar = "https://github.com/vkbo/novelWriter/archive/v1.1.1.tar.gz";
+  $nwStableVers   = "1.2";
+  $nwStableDate   = "2021-03-14";
+  $nwStableNotes  = "https://github.com/vkbo/novelWriter/releases/tag/v1.2";
+  $nwStableMinMac = "https://github.com/vkbo/novelWriter/releases/download/v1.2/novelWriter-1.2-minimal-darwin.zip";
+  $nwStableMinLnx = "https://github.com/vkbo/novelWriter/releases/download/v1.2/novelWriter-1.2-minimal-linux.zip";
+  $nwStableMinWin = "https://github.com/vkbo/novelWriter/releases/download/v1.2/novelWriter-1.2-minimal-win.zip";
+  $nwStableSrcZip = "https://github.com/vkbo/novelWriter/archive/v1.2.zip";
+  $nwStableSrcTar = "https://github.com/vkbo/novelWriter/archive/v1.2.tar.gz";
 
   // Latest Testing Release
-  $hasTestingvers  = true;
+  $hasTestingvers  = false;
   $nwTestingVers   = "1.2 RC 1";
   $nwTestingDate   = "2021-03-02";
   $nwTestingNotes  = "https://github.com/vkbo/novelWriter/releases/tag/v1.2rc1";
