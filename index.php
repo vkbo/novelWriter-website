8b015229ac3288cf8e14c3a109f5d1d36475080a
Veronica Berglyd Olsen <1619840+vkbo@users.noreply.github.com>
2021-07-27 23:55:12 +0200

    Release 1.4.1

--- index.php
+++ index.php
@@ -5,14 +5,14 @@
   // ========
 
   // Latest Stable Release
-  $nwStableVers   = "1.4";
+  $nwStableVers   = "1.4.1";
   $nwStableDate   = "2021-07-27";
-  $nwStableNotes  = "https://github.com/vkbo/novelWriter/releases/tag/v1.4";
-  $nwStableMinMac = "https://github.com/vkbo/novelWriter/releases/download/v1.4/novelWriter-1.4-minimal-darwin.zip";
-  $nwStableMinLnx = "https://github.com/vkbo/novelWriter/releases/download/v1.4/novelWriter-1.4-minimal-linux.zip";
-  $nwStableMinWin = "https://github.com/vkbo/novelWriter/releases/download/v1.4/novelWriter-1.4-minimal-win.zip";
-  $nwStableSrcZip = "https://github.com/vkbo/novelWriter/archive/refs/tags/v1.4.zip";
-  $nwStableSrcTar = "https://github.com/vkbo/novelWriter/archive/refs/tags/v1.4.tar.gz";
+  $nwStableNotes  = "https://github.com/vkbo/novelWriter/releases/tag/v1.4.1";
+  $nwStableMinMac = "https://github.com/vkbo/novelWriter/releases/download/v1.4.1/novelWriter-1.4.1-minimal-darwin.zip";
+  $nwStableMinLnx = "https://github.com/vkbo/novelWriter/releases/download/v1.4.1/novelWriter-1.4.1-minimal-linux.zip";
+  $nwStableMinWin = "https://github.com/vkbo/novelWriter/releases/download/v1.4.1/novelWriter-1.4.1-minimal-win.zip";
+  $nwStableSrcZip = "https://github.com/vkbo/novelWriter/archive/refs/tags/v1.4.1.zip";
+  $nwStableSrcTar = "https://github.com/vkbo/novelWriter/archive/refs/tags/v1.4.1.tar.gz";
 
   // Latest Testing Release
   $hasTestingvers  = false;
