25c9dc338831d3bd3c81eb0eca9157c250a9dbd3
Veronica Berglyd Olsen <1619840+vkbo@users.noreply.github.com>
2022-09-29 19:51:57 +0200

    Release 1.6.4

--- index.php
+++ index.php
@@ -5,17 +5,17 @@
   // ========
 
   // Latest Stable Release
-  $nwStableVers   = "1.6.3";
-  $nwStableDate   = "2022-08-18";
-  $nwStableNotes  = "https://github.com/vkbo/novelWriter/releases/tag/v1.6.3";
-  $nwStableMinMac = "https://github.com/vkbo/novelWriter/releases/download/v1.6.3/novelwriter-1.6.3-minimal-darwin.zip";
-  $nwStableMinLnx = "https://github.com/vkbo/novelWriter/releases/download/v1.6.3/novelwriter-1.6.3-minimal-linux.zip";
-  $nwStableMinWin = "https://github.com/vkbo/novelWriter/releases/download/v1.6.3/novelwriter-1.6.3-minimal-win.zip";
-  $nwStableDebian = "https://github.com/vkbo/novelWriter/releases/download/v1.6.3/novelwriter_1.6.3_all.deb";
-  $nwStableWinExe = "https://github.com/vkbo/novelWriter/releases/download/v1.6.3/novelwriter-1.6.3-py3.10.6-win10-amd64-setup.exe";
-  $nwStablePWheel = "https://github.com/vkbo/novelWriter/releases/download/v1.6.3/novelWriter-1.6.3-py3-none-any.whl";
-  $nwStableSrcZip = "https://github.com/vkbo/novelWriter/archive/refs/tags/v1.6.3.zip";
-  $nwStableSrcTar = "https://github.com/vkbo/novelWriter/archive/refs/tags/v1.6.3.tar.gz";
+  $nwStableVers   = "1.6.4";
+  $nwStableDate   = "2022-09-29";
+  $nwStableNotes  = "https://github.com/vkbo/novelWriter/releases/tag/v1.6.4";
+  $nwStableMinMac = "https://github.com/vkbo/novelWriter/releases/download/v1.6.4/novelwriter-1.6.4-minimal-darwin.zip";
+  $nwStableMinLnx = "https://github.com/vkbo/novelWriter/releases/download/v1.6.4/novelwriter-1.6.4-minimal-linux.zip";
+  $nwStableMinWin = "https://github.com/vkbo/novelWriter/releases/download/v1.6.4/novelwriter-1.6.4-minimal-win.zip";
+  $nwStableDebian = "https://github.com/vkbo/novelWriter/releases/download/v1.6.4/novelwriter_1.6.4_all.deb";
+  $nwStableWinExe = "https://github.com/vkbo/novelWriter/releases/download/v1.6.4/novelwriter-1.6.4-py3.10.7-win10-amd64-setup.exe";
+  $nwStablePWheel = "https://github.com/vkbo/novelWriter/releases/download/v1.6.4/novelWriter-1.6.4-py3-none-any.whl";
+  $nwStableSrcZip = "https://github.com/vkbo/novelWriter/archive/refs/tags/v1.6.4.zip";
+  $nwStableSrcTar = "https://github.com/vkbo/novelWriter/archive/refs/tags/v1.6.4.tar.gz";
 
   // Latest Testing Release
   $hasTestingvers  = true;
